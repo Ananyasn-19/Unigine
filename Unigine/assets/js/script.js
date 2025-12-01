b0ee209c8d949f93351e249c4f117b9c50b4935f
Ananyasn-19 <ananyasn37@gmail.com>
2025-12-01 19:12:56 +0530

    fourth  commit

---
+++ Unigine/assets/js/script.js
@@ -0,0 +1,14 @@
+'use script';
+
+
+
+/**
+ * add event on mutiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener
+    }
+
+}
